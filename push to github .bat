0c5b477997a9e8882f9fba5f224f8ce55414e404
Ammi Said <ammi.said.kaidi@gmail.com>
2026-03-16 20:32:49 +0100

    Initial commit for StockOS project

--- push to github .bat
+++ push to github .bat
@@ -1,5 +1,5 @@
 @echo off
-REM Initialize Git repository in current folder
+REM Initialize Git repository if not already done
 git init
 
 REM Add all files
@@ -8,8 +8,8 @@ git add .
 REM Commit with a message
 git commit -m "Initial commit for StockOS project"
 
-REM Link to your GitHub repository
-git remote add origin https://github.com/ammisaidkaidi/StockOS.git
+REM Update remote origin to your GitHub repo (HTTPS)
+git remote set-url origin https://github.com/ammisaidkaidi/StockOS.git
 
 REM Set branch name and push
 git branch -M main
